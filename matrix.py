46bbbde7d0b2585518e85d9eb6e8a2c542deafc1
mateusfch <mateus.charloto@edu.pucrs.br>
2024-04-05 16:16:34 -0300

    Matrix documentation

--- matrix.py
+++ matrix.py
@@ -1,35 +1,44 @@
 from collections import defaultdict
 from functions import getKmersSeq,getKmers, assign_family, assign_family_or_op, frequency_per_family
+import pandas as pd
 
+# Open and read the database file
 database = open("database/pr2_version_5.0.0_SSU_dada2.fasta", "r")
 db_rows  = database.readlines()
 
-organismos = defaultdict(int)
-count = defaultdict(int)
+# Initialize the datastructures
+db_organisms = defaultdict(int)
 all_families = set()
+count = defaultdict(int)
 
-dict = {}
-
+# Parse the database rows to extract organism information
 for i in range(0, len(db_rows ), 2):
   all_families.add(db_rows [i].split(";")[6])
   count[db_rows [i].split(";")[6]] +=1
-  organismos[db_rows [i].split(";")[6] + " " + str(count[db_rows [i].split(";")[6]])] = db_rows [i+1]
+  db_organisms[db_rows [i].split(";")[6] + " " + str(count[db_rows [i].split(";")[6]])] = db_rows [i+1]
 
-sequencia = input("Informe a sequência a ser classificada\n")
+# Get the sequence input from the user
+sequency = input("Enter the sequence to be assigned\n")
 
-k_cl = getKmersSeq(sequencia, 8)
-k_db = getKmers(organismos, 8)
+# Extract k-mers from the input sequence and database organisms
+k_cl = getKmersSeq(sequency, 8)
+k_db = getKmers(db_organisms, 8)
 
-classificacao = assign_family(k_cl, k_db)
-classificacao_or = assign_family_or_op(classificacao, k_cl, all_families)
+# Assign families based on the k-mers
+assignment = assign_family(k_cl, k_db)
 
-print("Assignment done!")
+# Assign families based on the k-mers - logical OR operation version
+assignment_or = assign_family_or_op(assignment, k_cl, all_families)
 
-# # writing OrOp Family matrix to Excel
-# df = (pd.DataFrame(classificacao_or)).transpose().sort_index(axis=1)
-# df['results'] = df.iloc[:, 1:].sum(axis=1)
-# df.sort_values(by=['results'],ascending=False, inplace=True)
+print("Assignment done\n")
 
-# print(df['results'].head(50))
+# Convert the assignment to a DataFrame for better visualization
+df = (pd.DataFrame(assignment_or)).transpose().sort_index(axis=1)
+df['Sumif 1'] = df.iloc[:, 1:].sum(axis=1)
+df.sort_values(by=['Sumif 1'],ascending=False, inplace=True)
 
-#df.to_excel('assignment.xlsx', index=True)
+# Print the top 50 results
+print(df['results'].head(50))
+
+# Export the DataFrame to an Excel file
+df.to_excel('assignment.xlsx', index=True)
